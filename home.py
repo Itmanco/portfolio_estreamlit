f41223ba9ab90e584ffc56ca67fcf33319719c02
Jaime Corredor <itmanco@gmail.com>
2023-04-03 06:16:59 +0900

    data updated

--- home.py
+++ home.py
@@ -12,10 +12,13 @@ with col1:
 with col2:
     st.title("Jaime Motta")
     content = """
-    Hi, I'm Jaime Motta, I'm a computer science engineer. Programming is my passion, I am currently working with python developing web applications.
-I am still discovering all the benefits that python provides as a programming language compared to my past experiences using c#, c++, Java.
-I like the idea of exploring the wide range of applications that can be implemented safely, quickly and easily in this language.
-I will continue experimenting and expanding the variety of applications available in the following list.
+    Hi, I Am Jaime Motta a computer science engineer. Programming is my passion, I am currently working with python 
+    developing web applications. I Am still discovering all the benefits that python provides as a programming language 
+    compared to my past experiences using c#, c++, Java.
+    I like the idea of exploring the wide range of applications that can be implemented safely, quickly and easily in 
+    this language.
+    
+    I will continue experimenting and expanding the variety of applications available in the following list.
     """
     st.write(content)
 
@@ -26,7 +29,7 @@ some of the following applications were created with the guidance of third parti
 """
 st.write(content2)
 
-col3, empty_column, col4 = st.columns([1.5, 0.5, 1.5])
+col3, empty_column, col4 = st.columns([1.2, 0.5, 1.2])
 
 df = pandas.read_csv("data.csv", sep=";")
 
